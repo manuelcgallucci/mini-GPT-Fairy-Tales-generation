1b08d8dcffe187bee657c5e9d8afa0ab11fefda3
Manuel Cabeza Gallucci <42854238+manuelcgallucci@users.noreply.github.com>
2023-11-27 22:58:35 -0300

    Update create_story.py

--- create_story.py
+++ create_story.py
@@ -1,18 +1,26 @@
 import torch
-from bigram_v6_scaleanddropout import BigramLanguageModel
+import json
+
+from bigram_model import *
+
+OUTPUT_DIR = "./stories_clean"
+META_DIR = os.path.join(OUTPUT_DIR, "meta.json")
 
 # Inference length
 story_length = 2000
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = BigramLanguageModel()
-m = model.to(device)
+model.to(device)
 
-with open("all_stories.txt", "r", encoding="utf-8") as f:
-    dataset = f.read()
+model.load_state_dict(torch.load("model.pt"))
+model.eval()
 
-chars = sorted(list(set(dataset)))
-vocab_size = len(chars)
+with open(META_DIR, "r") as f:
+    meta = json.load(f)   
+
+chars = meta["chars"]
+vocab_size = meta["vocab_size"]
 # Mapping from vocab chars to ints 
 # Very small vocal size tokenizer, gpt-2 uses 50k 
 int2str = {i:ch for i, ch in enumerate(chars)}
@@ -20,4 +28,4 @@ decode = lambda l: "".join([int2str[i] for i in l])
 
 context = torch.zeros((1, 1), device=device, dtype=torch.long)
 with open("output.txt", "w") as f:
-    f.write(decode(m.generate(context, max_new_tokens=story_length)[0].tolist()))
+    f.write(decode(model.generate(context, max_new_tokens=story_length)[0].tolist()))
